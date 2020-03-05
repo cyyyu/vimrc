f6e1a3a683a50a39ef0c7d7187bdbf91fdec4b78
Chuang Yu <cyu9960@gmail.com>
2020-03-05 10:53:12 +0800

    improve install script

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-# Install requirements
-read -p "Intall requirements[y/n]?(vim-plug): " yn
-if [ "$yn" == 'y' ]
+# Plug is required
+if [ ! -f "$HOME/.vim/autoload/plug.vim" ]
 then
+  echo "Installing plug..."
   curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
